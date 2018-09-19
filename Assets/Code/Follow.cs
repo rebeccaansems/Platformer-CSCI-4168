77171a0358452460e9ef4c28ecce5b24e7317997
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-18 22:26:33 -0300

    Created follow object

---
+++ Assets/Code/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+    }
+}
