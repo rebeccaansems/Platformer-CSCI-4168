de38af9d95ea38b6460d697d768515e6de58ddb2
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-18 21:17:43 -0300

    Robot can now move

---
+++ Assets/Code/Robot/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private new Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+    
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigidbody.MovePosition(transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, 
+            Input.GetAxis("Vertical")) * Time.deltaTime * moveSpeed);
+    }
+}
