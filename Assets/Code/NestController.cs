01200638682301564527fe33006b941034583be0
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-18 23:54:23 -0300

    Foxes can go to nest

---
+++ Assets/Code/NestController.cs
@@ -0,0 +1,37 @@
+﻿using MalbersAnimations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestController : MonoBehaviour
+{
+    public PlayerCharacter characterController;
+    public float maxDistanceToFollow;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        if (Vector3.Distance(transform.position, characterController.transform.position) < maxDistanceToFollow)
+        {
+            foreach(AnimalAIControl animal in characterController.animalsCurrentlyFollowing)
+            {
+                animal.GetComponent<FollowerController>().nestLocation = (this.transform);
+            }
+        }
+        else
+        {
+            Debug.Log("Get closer");
+        }
+    }
+}
---
+++ Assets/Code/Robot/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MalbersAnimations;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    public List<AnimalAIControl> animalsCurrentlyFollowing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
