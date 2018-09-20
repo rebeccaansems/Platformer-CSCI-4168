9375c6353fb08ac4be0ba611f756374f462c138d
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-20 16:44:26 -0300

    Prevent player from climbing walls

--- Assets/Code/Robot/PlayerControl.cs
+++ Assets/Code/Robot/PlayerControl.cs
@@ -9,7 +9,7 @@ public class PlayerControl : MonoBehaviour
 
     private new Rigidbody rigidbody;
     private Animator animator;
-    private float groundHeight;
+    private float groundHeight, currentMoveSpeed;
 
     private void Start()
     {
@@ -30,7 +30,16 @@ public class PlayerControl : MonoBehaviour
 
     private void Move()
     {
-        rigidbody.MovePosition(transform.position + transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+        currentMoveSpeed = moveSpeed;
+
+        Debug.Log(GetRaycastHit().normal + " " + GetRaycastHit().distance);
+        if (IsSlope())
+        {
+            currentMoveSpeed = moveSpeed * (1 - Mathf.Max(Mathf.Max(GetRaycastHit().normal.x, Mathf.Abs(GetRaycastHit().normal.x - 0.3f))));
+            Debug.Log(currentMoveSpeed);
+        }
+
+        rigidbody.MovePosition(transform.position + transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * currentMoveSpeed);
         transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed, 0);
     }
 
@@ -41,8 +50,18 @@ public class PlayerControl : MonoBehaviour
 
     private bool IsGrounded()
     {
+        return GetRaycastHit().distance < 0.71;
+    }
+
+    private bool IsSlope()
+    {
+        return GetRaycastHit().distance < 2 && (GetRaycastHit().normal.x >= 0.6f || GetRaycastHit().normal.x <= -0.3f);
+    }
+
+    private RaycastHit GetRaycastHit()
+    {
         RaycastHit hit = new RaycastHit();
         Physics.Raycast(baseOrb.position, -Vector3.up, out hit);
-        return hit.distance < 0.71;
+        return hit;
     }
 }
