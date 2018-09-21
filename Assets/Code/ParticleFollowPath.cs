26b0e33fc9d13463852c40ebfa2f94bcb2b82df9
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-21 03:06:03 -0300

    Water hose now goes continuously

---
+++ Assets/Code/ParticleFollowPath.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPath : MonoBehaviour
+{
+    public bool isPlaying;
+
+    private IEnumerator particleCorountine;
+
+    private void Start()
+    {
+        this.GetComponent<ParticleSystem>().Stop();
+    }
+
+    public void Play(string pathName, float time)
+    {
+        this.GetComponent<ParticleSystem>().Play();
+        isPlaying = true;
+        particleCorountine = PlayParticleSystem(pathName, time);
+        StartCoroutine(particleCorountine);
+    }
+
+    public void PlayOnce(string pathName, float time)
+    {
+        this.GetComponent<ParticleSystem>().Play();
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeOutQuint, "time", time));
+        this.GetComponent<ParticleSystem>().Stop();
+    }
+
+    public void Stop()
+    {
+        //this.GetComponent<ParticleSystem>().Stop();
+        //isPlaying = false;
+    }
+
+    IEnumerator PlayParticleSystem(string pathName, float time)
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeOutSine, "time", time, "looptype", iTween.LoopType.loop));
+        yield return new WaitForSeconds(0.1f);
+    }
+}
--- Assets/Code/Robot/PlayerWaterHose.cs
+++ Assets/Code/Robot/PlayerWaterHose.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class PlayerWaterHose : MonoBehaviour
 {
+    ParticleFollowPath[] allParticlePaths;
 
+    private void Awake()
+    {
+        foreach (iTweenPath path in GetComponentsInChildren<iTweenPath>())
+        {
+            path.pathName = path.gameObject.name;
+        }
+    }
+
+    private void Start()
+    {
+        allParticlePaths = GetComponentsInChildren<ParticleFollowPath>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && allParticlePaths[0].isPlaying == false)
+        {
+            StartCoroutine(PlayParticleSystem(2f));
+        }
+    }
+
+    IEnumerator PlayParticleSystem(float time)
+    {
+        foreach (ParticleFollowPath particlePath in allParticlePaths)
+        {
+            particlePath.Play(particlePath.transform.parent.gameObject.name, time);
+            yield return new WaitForSeconds(time / allParticlePaths.Length);
+        }
+    }
 }
