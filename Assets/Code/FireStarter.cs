385541dddc14adaea10bcae894193b0f26afc429
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-24 12:12:29 -0300

    Bushes are randomly set on fire at beginning of game

---
+++ Assets/Code/FireStarter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FireStarter : MonoBehaviour {
+
+    public Vector2 extremesForNumberOfFires;
+    public GameObject fire;
+
+    private List<GameObject> allBushes;
+    
+	void Awake () {
+        allBushes = GameObject.FindGameObjectsWithTag("Bush").ToList();
+
+        //sort bushes randomly
+        System.Random rnd = new System.Random();
+        allBushes = allBushes.OrderBy(item => rnd.Next()).ToList();
+
+        //set random number of random bushes on fire
+        int numberFires = Random.Range((int)extremesForNumberOfFires.x, Mathf.Min((int)extremesForNumberOfFires.y, allBushes.Count));
+        for(int i=0; i<numberFires; i++)
+        {
+            Instantiate(fire, allBushes[i].transform);
+            allBushes[i].AddComponent<GeneralFireObject>();
+            allBushes[i].transform.parent = this.transform;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Code/GeneralFireObject.cs
+++ Assets/Code/GeneralFireObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneralObject : MonoBehaviour
+public class GeneralFireObject : MonoBehaviour
 {
     private GameObject player;
     private FireController fire;
