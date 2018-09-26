b281e512c6dcab0d109557a01d1c5ded23cb7bbf
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-26 17:26:47 -0300

    Powerpacks now get sucked into player

---
+++ Assets/Code/PlayerFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public int followDistance;
+    public float speed;
+
+    private Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < followDistance)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(this.transform.position, player.position, step);
+        }
+    }
+}
