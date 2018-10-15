382255c0b16f5672e2ae7c130c0573ee49db9dad
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-10-15 20:07:48 -0300

    Added moving ice blocks

---
+++ Assets/Code/MoveBetweenPoints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenPoints : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 pointA, pointB;
+
+    private Vector3 goingToPoint;
+
+    private void Start()
+    {
+        goingToPoint = pointA;
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(goingToPoint, this.transform.position) < 0.1f)
+        {
+            if (goingToPoint == pointA)
+            {
+                goingToPoint = pointB;
+            }
+            else
+            {
+                goingToPoint = pointA;
+            }
+        }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, goingToPoint, speed * Time.deltaTime);
+    }
+}
